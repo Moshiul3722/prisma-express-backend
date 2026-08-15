6c5e11cb94727fcf6e1eccca28001beeb5a295a3
Moshiul <gazimoshiul@gmail.com>
2026-08-16 00:12:07 +0600

    21-9 Cleaning And Organizing the Auth Middleware and

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,6 +4,9 @@ import httpStatus from "http-status-codes";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import jwt from "jsonwebtoken";
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
 
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -21,6 +24,56 @@ const registerUser = catchAsync(
   },
 );
 
+const getMyProfile = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    // const { accessToken } = req.cookies;
+    // console.log(accessToken);
+
+    // console.log(req.user, "user request");
+    // const verifiedToken = jwtUtils.verifyToken(
+    //   accessToken,
+    //   config.jwt_access_secret,
+    // );
+
+    // if (typeof verifiedToken === "string") {
+    //   throw new Error(verifiedToken);
+    // }
+
+    const profile = await userService.getMyProfileFromDB(
+      req.user?.id as string,
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User profile fetched successfully",
+      data: { profile },
+    });
+
+    res.send("Get my profile");
+  },
+);
+
+const updateMyProfile = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user?.id as string;
+    const payload = req.body;
+    const updatedProfile = await userService.updateMyProfileFromDB(
+      userId,
+      payload,
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User profile updated successfully",
+      data: { updatedProfile },
+    });
+  },
+);
+
 export const userController = {
   registerUser,
+  getMyProfile,
+  updateMyProfile,
 };
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,9 +1,63 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 
 import { userController } from "./user.controller";
+import { jwtUtils } from "../../utils/jwt";
+import config from "../../config";
+import { Role } from "../../../generated/prisma/enums";
+import httpStatus from "http-status-codes";
+import { catchAsync } from "../../utils/catchAsync";
+import { JwtPayload } from "jsonwebtoken";
+import { prisma } from "../../lib/prisma";
+import { auth } from "../../middlewares/auth";
 
 const router = Router();
 
 router.post("/register", userController.registerUser);
+
+router.get(
+  "/me",
+  // (req: Request, res: Response, next: NextFunction) => {
+  //   console.log(req.cookies);
+
+  //   const { accessToken } = req.cookies;
+  //   console.log(accessToken);
+
+  //   const verifiedToken = jwtUtils.verifyToken(
+  //     accessToken,
+  //     config.jwt_access_secret,
+  //   );
+
+  //   if (!verifiedToken.success) {
+  //     throw new Error(verifiedToken.error);
+  //   }
+  //   const { name, id, email, role } = verifiedToken.data as JwtPayload;
+  //   const requiredRoles = [Role.ADMIN, Role.USER, Role.AUTHOR];
+
+  //   if (!requiredRoles.includes(role)) {
+  //     return res.status(403).json({
+  //       success: false,
+  //       statusCode: httpStatus.FORBIDDEN,
+  //       message:
+  //         "Forbidden. You don't have permission to access this resource.",
+  //     });
+  //   }
+
+  //   req.user = {
+  //     email,
+  //     name,
+  //     id,
+  //     role,
+  //   };
+  //   next();
+  // },
+  auth(Role.ADMIN, Role.USER, Role.AUTHOR),
+  userController.getMyProfile,
+);
+
+router.put(
+  "/my-profile",
+  auth(Role.ADMIN, Role.AUTHOR, Role.USER),
+  userController.updateMyProfile,
+);
 
 export const userRoutes = router;
